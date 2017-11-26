254ff0d91010b53dec9b87c8e5891ed5f3169aa4
Roman Figurin <r.v.figurin@gmail.com>
2017-11-26 02:51:50 +0200

    Xcode project was initiated

---
+++ hf_client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hf_client
+//
+//  Created by Roman Figurin on 26/11/2017.
+//  Copyright © 2017 Roman Figurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
